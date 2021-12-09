ddc9e7c5c4c49801df878edb25eb75431412a665
gehmac <gmarcoscardoso@gmail.com>
2021-12-09 15:55:33 -0300

    adiciona condicional para pesquisa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,24 +3,30 @@ import React from 'react';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-function Header({ title }) {
+function Header({ title, isSearchEnabled = false }) {
   return (
     <div>
       <header>
-        <button type="button" data-testid="profile-top-btn">
+        <button src={ profileIcon } type="button" data-testid="profile-top-btn">
           <img src={ profileIcon } alt="profile-Icon" />
         </button>
         <h1 data-testid="page-title">{ title }</h1>
-        <button type="button" data-testid="search-top-btn">
-          <img src={ searchIcon } alt="share-icon" />
-        </button>
+        { isSearchEnabled && (
+          <button src={ searchIcon } type="button" data-testid="search-top-btn">
+            <img src={ searchIcon } alt="share-icon" />
+          </button>)}
       </header>
     </div>
   );
 }
 
 Header.propTypes = {
+  isSearchEnabled: PropTypes.bool,
   title: PropTypes.string.isRequired,
+};
+
+Header.defaultProps = {
+  isSearchEnabled: false,
 };
 
 export default Header;
