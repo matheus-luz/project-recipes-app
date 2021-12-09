c2a2bacf53f4542b9f91f3ba3779df08c39cabe9
matheus <matheusfelippeh@gmail.com>
2021-12-09 17:35:33 -0300

    Adiciona rotas para os botoes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
+  const history = useHistory();
   return (
     <footer data-testid="footer">
       <button
         data-testid="drinks-bottom-btn"
         type="button"
         src={ drinkIcon }
+        onClick={ () => history.push('/bebidas') }
       >
         <img src={ drinkIcon } alt="drink-icon" />
       </button>
@@ -17,6 +20,7 @@ function Footer() {
         data-testid="explore-bottom-btn"
         type="button"
         src={ exploreIcon }
+        onClick={ () => history.push('/explorar') }
       >
         <img src={ exploreIcon } alt="explore-icon" />
       </button>
@@ -24,6 +28,7 @@ function Footer() {
         data-testid="food-bottom-btn"
         type="button"
         src={ mealIcon }
+        onClick={ () => history.push('/comidas') }
       >
         <img src={ mealIcon } alt="meal-icon" />
       </button>
