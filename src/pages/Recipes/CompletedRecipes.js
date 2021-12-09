ee165cb3ba78e2b803897c0fece40cb97bb2f5f4
gehmac <gmarcoscardoso@gmail.com>
2021-12-09 15:15:06 -0300

    adiciona componente funcional

---
+++ src/pages/Recipes/CompletedRecipes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CompletedRecipes() {
+  return (
+    <div />
+  );
+}
+
+export default CompletedRecipes;
---
+++ src/pages/Recipes/FavoriteRecipe.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FavoriteRecipe() {
+  return (
+    <div />
+  );
+}
+
+export default FavoriteRecipe;
