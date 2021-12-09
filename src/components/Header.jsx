c0991c4e62d18d6a798ecdcf52ef6e54afc71e8e
gehmac <gmarcoscardoso@gmail.com>
2021-12-09 16:16:19 -0300

    adiciona input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,37 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title, isSearchEnabled = false }) {
+  const history = useHistory();
+  const [isInput, setIsInput] = useState(false);
+
   return (
     <div>
       <header>
-        <button src={ profileIcon } type="button" data-testid="profile-top-btn">
+        <button
+          src={ profileIcon }
+          type="button"
+          data-testid="profile-top-btn"
+          onClick={ () => history.push('/perfil') }
+        >
           <img src={ profileIcon } alt="profile-Icon" />
         </button>
         <h1 data-testid="page-title">{ title }</h1>
         { isSearchEnabled && (
-          <button src={ searchIcon } type="button" data-testid="search-top-btn">
+          <button
+            src={ searchIcon }
+            type="button"
+            data-testid="search-top-btn"
+            onClick={ () => setIsInput(!isInput) }
+          >
             <img src={ searchIcon } alt="share-icon" />
           </button>)}
       </header>
+      {isInput
+      && <input data-testid="search-input" type="text" />}
     </div>
   );
 }
