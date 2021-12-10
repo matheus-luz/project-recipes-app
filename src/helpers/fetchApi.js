f3c04ce209b95010f91b95f703c19e470c959f5f
gehmac <gmarcoscardoso@gmail.com>
2021-12-10 16:10:22 -0300

    cria funções para api

--- src/helpers/fetchApi.js
+++ src/helpers/fetchApi.js
@@ -1,13 +1,24 @@
-export const INGREDIENT_URL = `https://www.${}.com/api/json/v1/1/filter.php?i=`;
-export const NAME_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-export const FIRST_LETTER_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
+export const FOOD_INGREDIENT = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
+export const FOOD_NAME_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+export const FOOD_FIRST_LETTER_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
 
-export function getLink(type, pathname) {
-  switch (type) {
-  case 'ingredient':
-    return INGREDIENT_URL
-  }
+export const DRINK_INGREDIENT = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+export const DRINK_NAME_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+export const DRINK_FIRST_LETTER_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
+
+export function getIngredientLink(pathname) {
+  return pathname.includes('comidas') ? FOOD_INGREDIENT : DRINK_INGREDIENT;
 }
+
+export function getNameLink(pathname) {
+  return pathname.includes('comidas') ? FOOD_NAME_URL : DRINK_NAME_URL;
+}
+
+export function getFirstLetterLink(pathname) {
+  return pathname.includes('comidas') ? FOOD_FIRST_LETTER_URL
+    : DRINK_FIRST_LETTER_URL;
+}
+
 export async function fetchApi(endpoint) {
   const response = await fetch(endpoint);
   const result = await response.json();
