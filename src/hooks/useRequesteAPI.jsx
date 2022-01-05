7dd906488671d9f4924e3591e959e7069e88f2a2
gehmac <gmarcoscardoso@gmail.com>
2022-01-05 09:43:12 -0300

    refatora Requete api

--- src/hooks/useRequesteAPI.jsx
+++ src/hooks/useRequesteAPI.jsx
@@ -4,15 +4,11 @@ function useRequesteAPI(API, ARRAY_SIZE) {
   const SEARCH_LIST = API;
   const [data, setData] = useState([]);
 
-  const getDataAPI = () => {
+  useEffect(() => {
     fetch(SEARCH_LIST)
       .then((response) => response.json())
-      .then((dataAPI) => setData(dataAPI.meals.slice(0, ARRAY_SIZE)));
-  };
-
-  useEffect(() => {
-    getDataAPI();
-  }, []);
+      .then((dataAPI) => setData(Object.values(dataAPI)[0].slice(0, ARRAY_SIZE)));
+  }, [SEARCH_LIST, ARRAY_SIZE]);
 
   return [data];
 }
