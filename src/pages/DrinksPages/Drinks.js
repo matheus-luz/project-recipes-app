8c4cdfb0bcca70a1c719e2dc2348c053c17014f0
gehmac <gmarcoscardoso@gmail.com>
2022-01-05 09:43:54 -0300

    adiciona comidas e bebidas em suas rotas

--- src/pages/DrinksPages/Drinks.js
+++ src/pages/DrinksPages/Drinks.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import CardsList from '../../components/CardsList';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import MyContext from '../../context/MyContext';
+import { DRINK_NAME_URL } from '../../helpers/fetchApi';
+import useRequesteAPI from '../../hooks/useRequesteAPI';
 
 function Drinks() {
+  const FOODS_SIZE = 12;
+  const [data] = useRequesteAPI(DRINK_NAME_URL, FOODS_SIZE);
+  const { setRequestAPI } = useContext(MyContext);
+
+  useEffect(() => {
+    setRequestAPI(data);
+  }, [setRequestAPI, data]);
+
   return (
     <div>
       <Header title="Bebidas" isSearchEnabled />
--- src/pages/FoodsPages/Foods.js
+++ src/pages/FoodsPages/Foods.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import CardsList from '../../components/CardsList';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
+import MyContext from '../../context/MyContext';
+import { FOOD_NAME_URL } from '../../helpers/fetchApi';
+import useRequesteAPI from '../../hooks/useRequesteAPI';
+
 function Foods() {
+  const FOODS_SIZE = 12;
+  const [data] = useRequesteAPI(FOOD_NAME_URL, FOODS_SIZE);
+  const { setRequestAPI } = useContext(MyContext);
+
+  useEffect(() => {
+    setRequestAPI(data);
+  }, [setRequestAPI, data]);
+
   return (
     <div>
       <Header title="Comidas" isSearchEnabled />
