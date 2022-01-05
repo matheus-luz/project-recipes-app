ea8e4c55859142a974850c3fbf33dfed4d3610df
Sayro <sayrodamaceno70@gmail.com>
2022-01-05 15:34:43 -0300

    implementa pagina de detalhes de bebidas e comidas

--- src/pages/DrinksPages/DrinksDetails.js
+++ src/pages/DrinksPages/DrinksDetails.js
@@ -1,8 +1,90 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { fetchDrinksRecipeByID } from '../../helpers/fetchApi';
 
 function DrinksDetails() {
+  const [recipe, setRecipe] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [ingredients, setIngredients] = useState([]);
+  const [measure, setMeasures] = useState([]);
+
+  const { id } = useParams();
+
+  const fetchRecipe = useCallback(
+    async () => {
+      const { drinks } = await fetchDrinksRecipeByID(id);
+      setRecipe(drinks[0]);
+      console.log(drinks[0]);
+      setIsLoading(false);
+    }, [id],
+  );
+
+  useEffect(() => {
+    fetchRecipe();
+  }, [fetchRecipe]);
+
+  const getIngredients = useCallback(
+    () => {
+      const ingredientsFiltered = Object.keys(recipe)
+        .filter((item) => item.includes('strIngredient'))
+        .filter((item) => recipe[item] !== null)
+        .map((item) => recipe[item]);
+      setIngredients(ingredientsFiltered);
+    }, [recipe],
+  );
+
+  useEffect(() => {
+    getIngredients();
+  }, [getIngredients]);
+
+  const getMeasures = useCallback(
+    () => {
+      const measuresFiltered = Object.keys(recipe)
+        .filter((item) => item.includes('strMeasure'))
+        .filter((item) => recipe[item] !== null)
+        .map((item) => recipe[item]);
+      setMeasures(measuresFiltered);
+    }, [recipe],
+  );
+
+  useEffect(() => {
+    getMeasures();
+  }, [getMeasures]);
+
+  const { strDrinkThumb, strDrink, strAlcoholic, strInstructions } = recipe;
+
+  if (isLoading) {
+    return (
+      <p>Loading...</p>
+    );
+  }
   return (
-    <div />
+    <div>
+      <img
+        data-testid="recipe-photo"
+        src={ strDrinkThumb }
+        alt={ strDrink }
+      />
+      <h3 data-testid="recipe-title">{ strDrink }</h3>
+      <button data-testid="share-btn" type="button">Share</button>
+      <button data-testid="favorite-btn" type="button">Favorite</button>
+      <p data-testid="recipe-category">{ strAlcoholic }</p>
+      <ol>
+        {ingredients.map((item, index) => (
+          <li
+            data-testid={ `${index}-ingredient-name-and-measure` }
+            key={ index }
+          >
+            {`${item} ${!measure[index] ? '' : `- ${measure[index]}`}`}
+          </li>
+        ))}
+      </ol>
+      <p data-testid="instructions">{ strInstructions }</p>
+
+      <p data-testid={ `${0}-recomendation-card` }>recomendation-card</p>
+
+      <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+    </div>
   );
 }
 
--- src/pages/FoodsPages/FoodsDetails.js
+++ src/pages/FoodsPages/FoodsDetails.js
@@ -1,8 +1,94 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { fetchMealsRecipeByID } from '../../helpers/fetchApi';
 
 function FoodsDetails() {
+  const [recipe, setRecipe] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [ingredients, setIngredients] = useState([]);
+  const [measure, setMeasures] = useState([]);
+
+  const { id } = useParams();
+
+  const fetchRecipe = useCallback(
+    async () => {
+      const { meals } = await fetchMealsRecipeByID(id);
+      setRecipe(meals[0]);
+      console.log(meals[0]);
+      setIsLoading(false);
+    }, [id],
+  );
+
+  useEffect(() => {
+    fetchRecipe();
+  }, [fetchRecipe]);
+
+  const getIngredients = useCallback(
+    () => {
+      const ingredientsFiltered = Object.keys(recipe)
+        .filter((item) => item.includes('strIngredient'))
+        .filter((item) => recipe[item] !== '')
+        .map((item) => recipe[item]);
+      setIngredients(ingredientsFiltered);
+    }, [recipe],
+  );
+
+  useEffect(() => {
+    getIngredients();
+  }, [getIngredients]);
+
+  const getMeasures = useCallback(
+    () => {
+      const measuresFiltered = Object.keys(recipe)
+        .filter((item) => item.includes('strMeasure'))
+        .filter((item) => recipe[item] !== ' ')
+        .map((item) => recipe[item]);
+      setMeasures(measuresFiltered);
+    }, [recipe],
+  );
+
+  useEffect(() => {
+    getMeasures();
+  }, [getMeasures]);
+
+  const { strMealThumb, strMeal, strCategory, strInstructions, strYoutube } = recipe;
+
+  if (isLoading) {
+    return (
+      <p>Loading...</p>
+    );
+  }
   return (
-    <div />
+    <div>
+      <img
+        data-testid="recipe-photo"
+        src={ strMealThumb }
+        alt={ strMeal }
+      />
+      <h3 data-testid="recipe-title">{ strMeal }</h3>
+      <button data-testid="share-btn" type="button">Share</button>
+      <button data-testid="favorite-btn" type="button">Favorite</button>
+      <p data-testid="recipe-category">{ strCategory }</p>
+      <ol>
+        {ingredients.map((item, index) => (
+          <li
+            data-testid={ `${index}-ingredient-name-and-measure` }
+            key={ index }
+          >
+            {`${item} - ${measure[index]}`}
+          </li>
+        ))}
+      </ol>
+      <p data-testid="instructions">{ strInstructions }</p>
+
+      <video data-testid="video" controls>
+        <source src={ strYoutube } type="video/mp4" />
+        <track src="" kind="captions" srcLang="en" label="English" />
+      </video>
+      <p data-testid={ `${0}-recomendation-card` }>recomendation-card</p>
+
+      <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+    </div>
   );
 }
 
