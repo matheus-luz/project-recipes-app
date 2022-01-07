1a67723b7079dee5f620496d4f2fac86ef9d1890
Sayro <sayrodamaceno70@gmail.com>
2022-01-07 15:20:03 -0300

    exibe email na tela de profile, e adiciona os botoes da tela

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const [emailFromLocalStorage, setEmailFromLocalStorage] = useState();
+
+  useEffect(() => {
+    const { email } = JSON.parse(localStorage.getItem('user'));
+    console.log(email);
+    setEmailFromLocalStorage(email);
+  }, []);
+
   return (
     <div>
       <Header title="Perfil" />
+
+      <p data-testid="profile-email">{ emailFromLocalStorage }</p>
+
+      <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
+      <button type="button" data-testid="profile-logout-btn">Sair</button>
       <Footer />
     </div>
   );
